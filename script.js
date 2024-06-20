3d67873af4efb76898bf1037573dfaafcd6faa04
d8rreon <darreonj08@gmail.com>
2024-06-20 14:37:34 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// JavaScript goes here.
+
+let question1 = document.querySelector(".q1button");
+let question2 = document.querySelector(".q2button");
+
+q1button.addEventListener("click", function({
+    let q1button = document.querySelector(".q1button").value
+}))
